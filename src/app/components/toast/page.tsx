be037cad651bb4744b36adb6301ffa894cfeb54c
nazaadimoha <nazaadimoha98@gmail.com>
2023-11-24 09:42:37 +0100

    bug fixes

--- src/app/components/toast/page.tsx
+++ src/app/components/toast/page.tsx
@@ -3,7 +3,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 type CloseButtonProps = {
-  closeToast: React.MouseEventHandler;
+  closeToast: any;
 };
 type PostToastProps = {
   message: string;
